fd58487b81733f64bb168f00697bee598e2c822f
Your Name <you@example.com>
2025-01-10 18:07:46 +0800

    update

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,7 @@ message_context = []
 def generate_message(userMessage):
     global message_context
     messages = [
-    SystemMessage("你是智能语音助手，回答用户的消息不超过200字;可以适当多加些语气词和停顿，不要有换行，不要有emoji"),
+    SystemMessage("你是智能语音助手，回答用户的消息不超过200字;可以适当多加些语气词,输出包含标点符号，不要有换行，不要有emoji"),
     ]
     message_context.append(HumanMessage(userMessage))
     messages += message_context
